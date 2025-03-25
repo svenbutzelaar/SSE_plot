b6fd9a0eb34231bb01329d0f6a3d2744fe4b96a1
Julian Hirschler <julian.j.hirschler@gmail.com>
2025-03-25 17:55:04 +0100

    add more datapoints to dataframes

--- data.py
+++ data.py
@@ -2,10 +2,10 @@ import numpy as np
 import pandas as pd
 
 
-def get_df_line():
+def get_df_line(num_points=10**4):
     df_line = pd.DataFrame({
-        "x": pd.date_range("2023-01-01", periods=100, freq="D"),
-        "y": np.random.normal(20, 5, 100).cumsum()  # Cumulative sum to simulate temperature trend
+        "x": pd.date_range("2023-01-01", periods=num_points, freq="D"),
+        "y": np.random.normal(20, 5, num_points).cumsum()  # Cumulative sum to simulate temperature trend
     })
     return df_line
 
@@ -19,30 +19,30 @@ def get_df_scatter():
 
 def get_df_bar():
     df_bar = pd.DataFrame({
-        "x": ["A", "B", "C", "D", "E"],
-        "y": [30, 45, 60, 80, 25]
+        "x": ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"],
+        "y": np.random.randint(20, 100, 10)
     })
     return df_bar
 
-def get_df_box():
+def get_df_box(num_points=10**7):
     df_box = pd.DataFrame({
-        "x": np.random.choice(["Group 1", "Group 2", "Group 3"], 100),
-        "y": np.random.normal(0, 1, 100)
+        "x": np.random.choice(["Group 1", "Group 2", "Group 3", "Group 4"], num_points),
+        "y": np.random.normal(0, 10, num_points)
     })
     return df_box
 
-def get_df_heatmap():
+def get_df_heatmap(num_rows=10**7):
     df_heatmap = pd.DataFrame({
-        "x": np.random.randn(100),
-        "y": np.random.randn(100),
-        "z": np.random.randn(100)
+        "x": np.random.randn(num_rows),
+        "y": np.random.randn(num_rows),
+        "z": np.random.randn(num_rows)
     })
-    df_heatmap_corr = df_heatmap.corr()  # Correlation matrix
+    df_heatmap_corr = df_heatmap.corr()
     return df_heatmap_corr
 
-def get_df_contour():
-    x = np.linspace(-5, 5, 100)
-    y = np.linspace(-5, 5, 100)
+def get_df_contour(num_points=10**7):
+    x = np.linspace(-5, 5, num_points)
+    y = np.linspace(-5, 5, num_points)
     X, Y = np.meshgrid(x, y)
     Z = np.sin(X**2 + Y**2)  # Example function for contour plot
 
