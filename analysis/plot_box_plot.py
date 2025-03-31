37730a047c92c08c2a2934c5c2452355c5d5cb83
Julian Hirschler <julian.j.hirschler@gmail.com>
2025-03-31 14:24:22 +0200

    add label to result plot

--- analysis/plot_box_plot.py
+++ analysis/plot_box_plot.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv("no_outliers_results.csv")
 
-selected_libraries = ["holoviews", "plotnine", "pygal", "seaborn"]
+selected_libraries = ["holoviews", "plotnine", "pygal", "seaborn", "plotly", "matplotlib"]
 
 df = df[df["library"].isin(selected_libraries)]
 
@@ -22,9 +22,12 @@ sns.boxplot(
     width=0.2, showfliers=False, order=order
 )
 
+for i, lib in enumerate(order):
+    plt.text(i, df[df["library"] == lib]["energy_delay_product"].max() * 1.05,
+             lib, ha="center", fontsize=9, color="black")
 plt.xlabel("Library")
 plt.ylabel("Energy Delay Product")
-plt.title("Energy Delay Product")
+plt.title("Energy Delay Product of Plotting Libraries")
 plt.xticks(rotation=15)
 plt.grid(axis="y", linestyle="--", alpha=0.5)
 plt.tight_layout()
