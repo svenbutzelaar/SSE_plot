1f9c843f4de943ef512a436c0d1de10866fe472a
Julian Hirschler <julian.j.hirschler@gmail.com>
2025-03-31 15:52:15 +0200

    add new energy score formular

--- analysis/energy_score.py
+++ analysis/energy_score.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 df = pd.read_csv("no_outliers_results.csv")
 
@@ -8,7 +9,11 @@ mean_energy.columns = ["library", "mean_energy_j"]
 E_min = mean_energy["mean_energy_j"].min()
 E_max = mean_energy["mean_energy_j"].max()
 
-mean_energy["energy_efficiency_score"] = 100 * (E_max - mean_energy["mean_energy_j"]) / (E_max - E_min)
+k = 2
+
+mean_energy["energy_efficiency_score"] = 100 * np.exp(
+    -k * ((mean_energy["mean_energy_j"] - E_min) / (E_max - E_min))
+)
 
 mean_energy = mean_energy.sort_values(by="energy_efficiency_score", ascending=False)
 
@@ -17,9 +22,9 @@ mean_energy.to_csv("energy_efficiency_scores.csv", index=False)
 
 # Output:
 #      library  mean_energy_j  energy_efficiency_score
-#  matplotlib     271.820368               100.000000 x
-#   holoviews    2483.148254                94.864212
-#     seaborn    3593.486141                92.285463
-#    plotnine   16961.212345                61.239046
-#       pygal   18897.745986                56.741465
-#      plotly   43329.047829                 0.000000 x
+#  matplotlib     271.820368               100.000000
+#   holoviews    2483.148254                90.238343
+#     seaborn    3593.486141                85.702281
+#    plotnine   16961.212345                46.060286
+#       pygal   18897.745986                42.097969
+#      plotly   43329.047829                13.533528
